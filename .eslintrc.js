72ff3f5c1163e5e0e3e76488cc29b6855286903e
Victor <zedfromionia@gmail.com>
2020-08-03 15:00:25 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        // commonjs: true,
+        node: true,
+        es2020: true,
+        jest: true
+    },
+    extends: ['airbnb-typescript/base'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        project: './tsconfig.json'
+    },
+    rules: {
+        'no-console': 0,
+        semi: 'off',
+        'no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'arrow-body-style': 'warn',
+        'linebreak-style': ['off', 'windows'],
+        indent: ['error', 4],
+        'comma-dangle': 'off'
+    }
+};

855858889d65af6d04c2b6c266f108888b841956
shingravirei <zedfromionia@gmail.com>
2021-03-31 00:36:07 -0300

    remove airbnb eslint config, bump deps, add plugin estling plugin import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,21 @@
 module.exports = {
     env: {
-        // commonjs: true,
+        commonjs: true,
         node: true,
         es2020: true,
         jest: true
     },
-    extends: ['airbnb-typescript/base'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript'
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2020,
         project: './tsconfig.json'
     },
-    rules: {
-        'no-console': 0,
-        semi: 'off',
-        'no-unused-vars': 'warn',
-        'arrow-body-style': 'warn',
-        'linebreak-style': ['off', 'windows'],
-        indent: ['error', 4],
-        'comma-dangle': 'off',
-        '@typescript-eslint/indent': 'off',
-        '@typescript-eslint/no-unused-vars': 'warn'
-    }
+    rules: {}
 };

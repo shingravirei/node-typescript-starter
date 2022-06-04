53951141f3e097628816e227b7186fed23c0dd15
shingravirei <42350757+shingravirei@users.noreply.github.com>
2022-06-04 12:23:30 -0300

    feat: move tests dir, add import aliases

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,10 +1,10 @@
+import { greeter } from '@/main';
 import { expect, test } from 'vitest';
-import greeter from '../src/main';
 
 test('Index', () => {
 	const greeting = greeter('Test');
 
 	expect(greeting).toBe('Hello, Test');
 
-	expect(greeter('error')).not.toBe('Hello,Test');
+	expect(greeter('error')).not.toBe('Hello, Test');
 });
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-export default (name: string): string => `Hello, ${name}`;
+export const greeter = (name: string): string => `Hello, ${name}`;

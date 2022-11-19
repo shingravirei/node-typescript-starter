2f18f1ae4bdcdef4b889610ef04fb231c2c06267
shingravirei <42350757+shingravirei@users.noreply.github.com>
2022-11-19 13:19:06 -0300

    feat: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,29 @@
 module.exports = {
-    env: {
-        node: true,
-        es2021: true,
-        jest: true,
-    },
-    plugins: ['@typescript-eslint'],
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        project: ['./tsconfig.json', 'testes/**/*'],
-    },
-    rules: {},
-    ignorePatterns: [
-        'node_modules',
-        'dist',
-        'migrations',
-        'jest.config.js',
-        '.eslintrc.js',
-        'prettier.config.js',
-        'tests',
-    ],
+	env: {
+		node: true,
+		es2022: true,
+	},
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:unicorn/recommended',
+		'prettier',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', 'testes/**/*'],
+	},
+	rules: {},
+	ignorePatterns: [
+		'node_modules',
+		'dist',
+		'migrations',
+		'jest.config.js',
+		'.eslintrc.js',
+		'prettier.config.js',
+		'tests',
+	],
 };
